92f945ae7bdde3cfb38596b4e5a1f5345f028dd3
Shin <Secret>
2021-12-28 11:30:37 +0900

    first commit

---
+++ src/main/java/com/loginapi/loginpakage/LoginpakageApplication.java
@@ -0,0 +1,13 @@
+package com.loginapi.loginpakage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginpakageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginpakageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loginapi/loginpakage/LoginpakageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginapi.loginpakage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginpakageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
